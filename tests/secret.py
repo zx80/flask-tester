4efaab891df7dedbf136d438e80ac7a783da7a00
Fabien Coelho <fabien@coelho.net>
2024-05-20 17:09:25 +0200

    simpler code

--- tests/secret.py
+++ tests/secret.py
@@ -4,17 +4,16 @@ import os
 import random
 import string
 
-# parameters
-LENGTH = 16
-CHARS = string.printable.replace(",", "")  # all but ","
-USERS = ["calvin", "hobbes", "susie", "moe"]
-
-# login -> clear-password
-PASSES: dict[str, str] = {}
-
 # both client and server must share the same seed!
 random.seed(os.environ.get("TEST_SEED", None))
 
-# generate 4 users with pseudo-random 16-chars passwords
-for login in USERS:
-    PASSES[login] = "".join(random.choice(CHARS) for _ in range(LENGTH))
+# parameters
+LENGTH: int = 16
+CHARS: str = string.printable.replace(",", "")  # all but ","
+USERS: list[str] = ["calvin", "hobbes", "susie", "moe"]
+
+# login -> clear-password
+PASSES: dict[str, str] = {
+    login: "".join(random.choice(CHARS) for _ in range(LENGTH))
+        for login in USERS
+}
