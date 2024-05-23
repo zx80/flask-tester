28986184c6dce6d338eceb93c847c809f623e76d
Fabien Coelho <fabien@coelho.net>
2024-05-23 10:55:31 +0200

    improve code & comments

--- tests/secret.py
+++ tests/secret.py
@@ -8,12 +8,14 @@ import string
 random.seed(os.environ.get("TEST_SEED", None))
 
 # parameters
-LENGTH: int = 16
-CHARS: str = string.printable.replace(",", "")  # all but ","
+PASS_LENGTH: int = 16
+PASS_CHARS: str = string.printable.replace(",", "")  # all but ","
+
+# user logins to generate
 USERS: list[str] = ["calvin", "hobbes", "susie", "moe"]
 
 # login -> clear-password
 PASSES: dict[str, str] = {
-    login: "".join(random.choice(CHARS) for _ in range(LENGTH))
+    login: "".join(random.choice(PASS_CHARS) for _ in range(PASS_LENGTH))
         for login in USERS
 }
